e9621b21eda1b10bc97182230f3a56eaa3b3244b
jaiBaynes <82232773+jaiBaynes@users.noreply.github.com>
2025-11-17 20:54:08 -0500

    adds background and sticky menu bar

--- New folder/gallery_site/db/seeds.rb
+++ New folder/gallery_site/db/seeds.rb
@@ -76,15 +76,15 @@ characters_gallery = Gallery.create!(
 hades = Image.create!(
   gallery: characters_gallery,
   name: "Hades",
-  quote: "TODO",
-  family: "Persephone (wife), Hecate ('sister'), Nyx (ex-lover), Cerberus (adoptive daughter), Kat (adoptive daughter), Python (adoptive son)",
-  biography: "TODO",
+  quote: "A myth is a fiction, a king is a despot and a hero is anything but a hero is anything but a glory-hungry, bloodthirsty, rapacious evildoer...",
+  family: "Terra Solaris/ Persephone (wife), Hecate ('sister'), Nyx (ex-lover), Cerberus (adoptive daughter), Kat (adoptive daughter), Python (adoptive son)",
+  biography: "According to Olympus, Hades is the god of the 'Underworld' and arch-rival of Zeus. In truth he is a once obscure god of the Molossian mines that rose in prominance after marrying Terra Solaris. With his new clout he founded his own nation, the first democracy: Elysium and his since then fought for his ideals and to expand Elysium's prosperity across the Kosmos, no matter how much it violates the 'fated role' Hades is supposed to play.",
   image_url: "/images/characters/hades.png",
   artist: "Me (Placeholder), Bentejam (BENTE), the.creature.keeper (Patricio Perez), akkurara (ramsha)",
   myth_inspiration: "In Mythology,Hades is the god of the underworld and the king of the dead. He is the son of Cronus and Rhea and the brother of Zeus and Poseidon. He is married to Persephone. A thing that interested me was the fact that according to some versions of myth, Hades was sterile and had no children. So I thought it would be cool to have Hades adopt lots of children and love them all despite no blood connection as a contrast to Zeus who has many biological children but is a complete asshole to most of them. Hades' character being focused on fatherhood is one of the many inspirtions I took from Odin, the Norse 'Allfather' as a fellow death god who I mixed with Hades. Third major inspiration from myth is the clever Greek hero Odysseus as a more positive reflection of the idea that the devil is an unreliable trickster(Zeus = Agamemnon, Poseidon = Diomedes and Hades = Odysseus).",
   powers: "Invisibility, Intangibility, Hellfire, Cthonic manipulation, Reanimation of the dead,Extra Lives, Fate Prediction",
   home: "Elysium",
-  personality: "TODO",
+  personality: "Clever, Cunning, Determineed/ Stubborn, Kind, Modest and Understated, Fatherly, Wise, Doting, Repentant",
   first_appearance: "Act 1",
   race: "Molossian Cthonic God (Immortal)",
   goal: "For the world to be a just and peaceful place... and for people to believe him when he says that."
@@ -103,7 +103,7 @@ cerberus = Image.create!(
   myth_inspiration: "In mythology, Cerberus is the three headed dog that guards the entrance to the underworld. Most know that Cerberus is the son of Typhon and Echidna and but few know that he has a 2 headed dog brother named Orthrus. This Cerberus adaptation is a 'What-If' Hades adopted yet another multi-headed dog sibling, this time female instead of two headed. As the adopted daughter of Hades, this Cerberus is also heavily influenced by Athena, the daughter of Zeus.",
   powers: "Invisibility, Intangibility, Shapeshifting, Shadow Manipulation, Familiar Creation,Teleportation, Hellfire, Microcosm: Doghouse, Extra Lives",
   home: "Underworld",
-  personality: "TODO",
+  personality: "Loyal, Stoic, Protective/ Overprotective, Brave, Secretly Doting, Concerned with image and reputation",
   first_appearance: "Act 1",
   race: "Demi-human Monster (Immortal)",
   goal: "To protect her home the Underworld and everyone in it."
@@ -122,7 +122,7 @@ kat = Image.create!(
   myth_inspiration: "No direct mythological basis; original character. Instead Kat is based on the concept of Schroddingers Cat: a cat that is both alive and dead until it is observed. Since her twin brother Python is a parallel to Apollo, Kat has some parallels to Artemis (Apollo's twin sister).",
   powers: "Invisibility, Intangibility, Probability Offseting, Life and Death, Superposition, Teleportation, Hellfire",
   home: "Elysium",
-  personality: "TODO",
+  personality: "Kind, Clever, Extroverted, Nerdy, Passionate, Idolizes Others/ lacks true self confidence, Nosy",
   first_appearance: "Act 1",
   race: "Demi-human Monster (Immortal)",
   goal: "To become a beloved Kosmonarch just like her father Hades."
@@ -133,7 +133,7 @@ load_character_images(kat)
 python = Image.create!(
   gallery: characters_gallery,
   name: "Python",
-  quote: "Every loop leads somewhere new",
+  quote: "I am a genius!",
   family: "Kat (twin sister), Cerberus (older sister), Hades (adoptive father), Typhon (biological father), Echidna (biological mother)",
   artist: "Meiko (meikkochi_), the.creature.keeper (Patricio Perez)",
   myth_inspiration: "Python the original god of delphi that took the form of a giant snake. Over time the character of Python went from being a female goddess with future sight to being reimagined as the evil monster brother of Typhon.",
@@ -141,7 +141,7 @@ python = Image.create!(
   home: "Elysium",
   biography: "Python is an innovative and forward-thinking character who thrives on continuous learning and adaptation. With a natural gift for seeing patterns and connections, Python excels at breaking down complex problems into manageable parts. Their flexibility and quick thinking make them an asset in any situation.",
   image_url: "/images/characters/python.png",
-  personality: "TODO",
+  personality: "Arrogant, bratty, insecure, self-centered, Idolizes others, petty, dense/ ignorant of feelings",
   first_appearance: "Act 1",
   race: "Humanoid/ Monster (Immortal)",
   goal: "To be popular and cool."
@@ -152,7 +152,7 @@ load_character_images(python)
 megaera = Image.create!(
   gallery: characters_gallery,
   name: "Megaera",
-  quote: "TODO",
+  quote: "Injustice anywhere is a threat to justice everywhere.",
   family: "Nyx (mother), Erebus (father), Tisiphone (triplet sister), Alecto (triplet sister), Thanatos (brother), Hypnos (brother)",
   biography: "Megaera is one of the three Furies, the daughters of Nyx, the goddess of the night. She is the leader of the Furies and is known for her fiery temper and ruthless pursuit of justice. Megaera is the chief enforcer of Elysium, pursuing and punishing the worst of the worst both within and without the boundaries of Elysium. She is extremely close to Cerberus.",
   image_url: "/images/characters/megaera.png",
@@ -160,7 +160,7 @@ megaera = Image.create!(
   myth_inspiration: "Megaera the leader of the Furies and daughter of Nyx, the goddess of the night. In some art work Nyx was depicted with skin that was dark blue or black (the literal color) so I thought it would be cool to have Megaera have black (as in African American) skin to reflect this. Her clothes are the starry night sky itself.",
   powers: "Shapeshifting, Shadow Manipulation, Familiar Creation, Hellfire, Morpheus Familiar, Microcosm: Nightmare, Fury Fusion",
   home: "Underworld",
-  personality: "TODO",
+  personality: "Aggressive, boistrous, friendly, passionate, extreme love or hate, humorous, eccentric/ insane, sadistic",
   first_appearance: "Bonus Chapters",
   race: "Goddess/ Child of Nyx (Immortal)",
   goal: "To protect her home the Underworld and punish all evildoers who threaten people everywhere."
