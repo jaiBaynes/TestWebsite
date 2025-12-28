26f6a8ffe6c8de71a23af56c4603fd3b287d6156
Jaiden-Baynes <baynesj@mcmaster.ca>
2025-12-28 02:54:00 -0500

    cleanup of deprecated files.

--- New folder/gallery_site/Boss Fights/test_hades_minions.py
+++
@@ -1,6 +0,0 @@
-"""Deprecated: replaced by the parametrized pytest `test_minions_param.py`.
-
-This file is kept to avoid surprises in scripts that reference it.
-"""
-
-# Run `pytest -q` to run the consolidated minion tests in `test_minions_param.py`.
--- New folder/gallery_site/Boss Fights/test_minions.py
+++
@@ -1,6 +0,0 @@
-"""Deprecated: replaced by the parametrized pytest `test_minions_param.py`.
-
-Keep this file only for backwards compatibility.
-"""
-
-# Use pytest runner on `test_minions_param.py` instead. (This file is intentionally minimal.)
